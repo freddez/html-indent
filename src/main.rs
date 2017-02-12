4d8d8dedfe413fa9b5e968d9d346f0d549c83df3
Frederic de Zorzi <fredz@pimentech.fr>
2017-02-12 16:53:19 +0200

    bourrow sorow

--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,52 @@
 extern crate regex;
 use regex::Regex;
 use std::env;
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::error::Error;
+use std::io::{Result, Read, BufRead, BufReader, Lines};
 
-pub struct Html {
-    s: String,
-    i: usize,
-    len: usize,
+pub struct Html<A: Read> {
+    lines: Lines<BufReader<A>>,
     indent_level: usize
 }
 
-impl Html {
-    fn new(path: &Path) -> Html {
-        let display = path.display();
-        let mut file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", display,
-                               why.description()),
-            Ok(file) => file,
-        };
-        //let mut content = String::new();
-        //let mut content;
-        let mut content = String::new();
-        match file.read_to_string(&mut content) {
-            Err(why) => panic!("couldn't read {}: {}", display,
-                               why.description()),
-            Ok(_) => Html {
-                s: content,
-                i: 0,
-                len:0,
-                indent_level: 0,
-            },
+impl<A: Read> Html<A> {
+    fn new(fd: A) -> Html<A> {
+        Html {
+            lines: BufReader::new(fd).lines(),
+            indent_level: 0,
         }
-        
     }
 
-    fn indent(&mut self) {
-        self.len = self.s.len();
-        //let out = String::new();
-        // let mut stack = Vec::new();
-        while self.i < self.len {
-            self.i = self.ltrim();
-            println!("i : {}",self.i);
-            
-            break;
-        }
-        // return &out;
+    fn write(&mut self, s: &str) {
+        print!("{}", s);
     }
-
-    fn ltrim(&mut self) -> usize {
-        lazy_static! {
-            static ref RSPACE: Regex = Regex::new(r"[^ ]").unwrap();
+    
+    fn indent(mut self) {
+        for (num, line) in self.lines.enumerate() {
+            for word in line.unwrap().split_whitespace() {
+                self.write(&word);
+            }
         }
-        let nospace = RSPACE.find(&self.s[self.i..]).unwrap();
+    }
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() <= 1 {
+        println!("No file specified");
+        return;
+    }
+    let path = Path::new(&args[1]);
+    let display = path.display();
+    println!("The first argument is {}", args[1]);
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,
+                           why.description()),
+        Ok(file) => file,
+    };
+    let buf = BufReader::new(file);
+    let mut htmlp = Html::new(buf);
+}
