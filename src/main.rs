bc593fe7df8811fe65b31a588eb861ada7b438db
Frederic de Zorzi <fredz@pimentech.fr>
2017-02-17 22:28:54 +0200

    approaching

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,23 @@ impl Html {
     fn write(&self, s: &str) {
         print!("{}", s);
     }
+    fn writeln(&self, s: &str) {
+        println!("{}", s);
+    }
     
     fn write_indent(&self) {
-        for i in 0..self.indent_level {
+        for _ in 0..self.indent_level {
             print!(" ");
         }
     }
     
     fn indent(&mut self) {
         lazy_static! {
-            static ref TAG: Regex = Regex::new("<(\"[^\"]*\"|'[^']*'|[^'\">])*>").unwrap();
+            static ref TAG: Regex = Regex::new(
+                "<(?P<closing>/)?(?P<name>\\w+)(?P<attrs>(\"[^\"]*\"|'[^']*'|[^'\">])*)?>"
+            ).unwrap();
         }
-        let mut tag_stack = Vec::<String>::new();
-        let mut i = 0;
+        // let mut tag_stack = Vec::<String>::new();
         let path = Path::new(&self.path);
         let display = path.display();
         let mut file = match File::open(&path) {
@@ -45,14 +49,49 @@ impl Html {
         };
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
-        let mut start = 0;
-        let mut end = 0;
-        for tag in TAG.find_iter(&content) {
-            start = tag.start();
-            end = tag.end();
-            println!("Tag : {} {} {}", start, end, tag.as_str());
+        let mut i=0;
+        for tag in TAG.captures_iter(&content) {
+            let tag_start = tag.get(0).unwrap().start();
+            let tag_end = tag.name("attrs").unwrap().end() + 1;
+            let txt = &content[i..tag_start].to_string();
+            // println!("TXT [{}]", txt);
+            // println!("TAG [{}]", &content[tag_start..tag_end].to_string());
+            // println!("-->> {:?}", tag.name("name").unwrap().as_str());
+            // println!("->> {} -> {} ", tag_start, tag_end);
+            let mut iter_lines = txt.lines();
+            //let mut line = iter_lines.next();
+            let mut n = 0;
+            loop { // UGLY, I DON'T KNOW HOW TO CATCH LAST ITERATION
+                print!("{}", n);
+                n += 1;
+                let line = iter_lines.next();
+                match line {
+			        Some(expr) => self.write(expr.trim()),
+			        None => break,
+		        }
+                self.writeln("$");
+                self.write_indent();
+
+                // match line {
+			    //     Some(expr) => {
+                //         println!("");
+                //         self.write_indent();
+                //     },
+                //     None => continue,
+		        // }
+            }                
+            if tag.name("closing").is_none() {
+                self.indent_level = self.indent_level + 2;
+            }
+            else {
+                self.indent_level = self.indent_level - 2;
+            }
+            print!("(");
+            self.write(&content[tag_start..tag_end].to_string().trim());
+            print!(")");
+            i = tag_end;
         }
-        
+        self.writeln("");
     }
 }
 
