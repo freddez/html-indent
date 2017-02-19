45d79c0f37ebcb5d44381e06c58a503c4b8b910b
Frederic de Zorzi <fredz@pimentech.fr>
2017-02-19 23:01:15 +0200

    tombé en marche

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,14 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+
 pub struct Html {
     path: String,
     indent_level: usize
 }
+
+
+
 
 impl Html {
     fn new(path: String) -> Html {
@@ -29,7 +33,12 @@ impl Html {
     
     fn write_indent(&self) {
         for _ in 0..self.indent_level {
-            print!(" ");
+            print!("_");
+        }
+    }
+    fn write_indent2(&self) {
+        for _ in 0..self.indent_level {
+            print!(".");
         }
     }
     
@@ -39,7 +48,6 @@ impl Html {
                 "<(?P<closing>/)?(?P<name>\\w+)(?P<attrs>(\"[^\"]*\"|'[^']*'|[^'\">])*)?>"
             ).unwrap();
         }
-        // let mut tag_stack = Vec::<String>::new();
         let path = Path::new(&self.path);
         let display = path.display();
         let mut file = match File::open(&path) {
@@ -50,45 +58,67 @@ impl Html {
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
         let mut i=0;
+        let mut after_newline = false;
         for tag in TAG.captures_iter(&content) {
             let tag_start = tag.get(0).unwrap().start();
             let tag_end = tag.name("attrs").unwrap().end() + 1;
             let txt = &content[i..tag_start].to_string();
-            // println!("TXT [{}]", txt);
-            // println!("TAG [{}]", &content[tag_start..tag_end].to_string());
-            // println!("-->> {:?}", tag.name("name").unwrap().as_str());
-            // println!("->> {} -> {} ", tag_start, tag_end);
-            let mut iter_lines = txt.lines();
-            //let mut line = iter_lines.next();
-            let mut n = 0;
-            loop { // UGLY, I DON'T KNOW HOW TO CATCH LAST ITERATION
-                print!("{}", n);
-                n += 1;
-                let line = iter_lines.next();
-                match line {
-			        Some(expr) => self.write(expr.trim()),
-			        None => break,
-		        }
-                self.writeln("$");
-                self.write_indent();
-
-                // match line {
-			    //     Some(expr) => {
-                //         println!("");
-                //         self.write_indent();
-                //     },
-                //     None => continue,
-		        // }
-            }                
+            let mut iter_lines = txt.split("\n");
+            let mut line = iter_lines.next();
+            loop {
+                let next = iter_lines.next();
+                match line {                    
+                    Some("") => {
+                        print!("");
+                        match(next) {
+                            Some(expr) => {
+                                self.writeln("");
+                                after_newline = true;
+                                line = next;
+                            },
+                            None => {
+                                break;
+                            }
+                        }
+                    }
+                    Some(expr) => {
+                        if after_newline {
+                            self.write_indent2();
+                            after_newline = false;
+                        }
+                        self.write(expr.trim());
+                        match(next) {
+                            Some(expr) => {
+                                self.writeln("");
+                                after_newline = true;
+                                line = next;
+                            },
+                            None => {
+                                break;
+                            }
+                        }
+                    }
+                    None => {
+                        break;
+                    }
+                }
+            }
+            
             if tag.name("closing").is_none() {
+                if after_newline {
+                    self.write_indent();
+                }
+                self.write(&content[tag_start..tag_end].to_string().trim());
                 self.indent_level = self.indent_level + 2;
             }
             else {
                 self.indent_level = self.indent_level - 2;
+                if after_newline {
+                    self.write_indent();
+                }
+                self.write(&content[tag_start..tag_end].to_string().trim());
             }
-            print!("(");
-            self.write(&content[tag_start..tag_end].to_string().trim());
-            print!(")");
+            after_newline = false;
             i = tag_end;
         }
         self.writeln("");
@@ -96,13 +126,14 @@ impl Html {
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() <= 1 {
-        println!("No file specified");
-        return;
-    }
-    let path = args[1].clone();
+    // let args: Vec<_> = env::args().collect();
+    // if args.len() <= 1 {
+    //     println!("No file specified");
+    //     return;
+    // }
+    // let path = args[1].clone();
+    let path = "/home/fredz/src/html-indent/test.kk";
     println!("The first argument is {}",path);
-    let mut htmlp = Html::new(path);
+    let mut htmlp = Html::new(path.to_string());
     htmlp.indent();
 }
